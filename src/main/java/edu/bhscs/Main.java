68fc4c27dee33821b6d5740bd900f3296bf484f6
2002354 <116846298+2002354@users.noreply.github.com>
2025-09-12 17:00:01 +0000

    My New Game

--- src/main/java/edu/bhscs/Main.java
+++ src/main/java/edu/bhscs/Main.java
@@ -15,7 +15,9 @@ package edu.bhscs;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Program Start");
-		System.out.println("My new message");
+		System.out.println("Shadowcast (3T)");
+		System.out.println("Rush - If this card resolves the turn it was flipped,");
+		System.out.println("You may add a card with cost 3T or less from your hand to your chain. (Cards added after time calculation don't cost any time.)");
 		System.out.println("Program End");
 	}
 }
