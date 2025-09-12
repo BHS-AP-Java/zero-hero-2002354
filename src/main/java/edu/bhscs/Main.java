dfc47371da3d7c6049e66aa5ac723dfd1c9a5f54
2002354 <116846298+2002354@users.noreply.github.com>
2025-09-12 17:01:55 +0000

    yeah

--- src/main/java/edu/bhscs/Main.java
+++ src/main/java/edu/bhscs/Main.java
@@ -17,7 +17,7 @@ public class Main {
 		System.out.println("Program Start");
 		System.out.println("Shadowcast (3T)");
 		System.out.println("Rush - If this card resolves the turn it was flipped,");
-		System.out.println("You may add a card with cost 3T or less from your hand to your chain. (Cards added after time calculation don't cost any time.)");
+		System.out.println("You may add a card with cost 3[T] or less from your hand to your chain. (Cards added after time calculation don't cost any time.)");
 		System.out.println("Program End");
 	}
 }
