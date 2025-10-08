897b26a9b181818a93b6d54407602603ec69ae45
2002354 <116846298+2002354@users.noreply.github.com>
2025-10-08 15:18:45 +0000

    k

--- src/main/java/edu/bhscs/Player.java
+++ src/main/java/edu/bhscs/Player.java
@@ -3,12 +3,12 @@ package edu.bhscs;
 public class Player {
   int hp;
 
-  public Player(int hp){
+  public Player(int hp) {
     this.hp = hp;
   }
 
   // just use scanner.nextLine() this method is kind of useless
-  public String AskQuestion(){
+  public String AskQuestion() {
     return Main.scanner.nextLine();
   }
 }
